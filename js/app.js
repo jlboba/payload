99e3872063de8dc78e51f964240a02b69e6501b1
Jerrica Bobadilla <Jerrica@Jerricas-MacBook-Pro.local>
2017-03-25 13:03:36 -0700

    player and computer objects created and player can now choose between attack or defend

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
 $(function() { // start window onload
-  console.log('app.js & window onload connected');
+
+  // EVENT LISTENERS
+  $('.position').on('click', gameInitialization.selectPosition);
+
 }) // end window onload
+
+// OBJECTS
+
+var gameVariables = {
+  'payload': 0
+}
+
+var gameInitialization = {
+  selectPosition: function() {
+    var position = $(this).attr('id');
+    if(position === '#attack') {
+      player.position = 'attack';
+      computer.position = 'defense';
+    } else {
+        computer.position = 'attack';
+        player.position = 'defense';
+    }
+  }
+}
+
+var player = {
+  'position': '',
+  'hero': '',
+  'health': 0,
+  'accuracy': 0,
+  'defense': 0,
+  'ultimate': 0
+}
+
+var computer = {
+  'position': '',
+  'hero': '',
+  'health': 0,
+  'accuracy': 0,
+  'defense': 0,
+  'ultimate': 0
+}
